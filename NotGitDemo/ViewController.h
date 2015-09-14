44442eeaa651f1a90850067fdfc50831beeaaf1d
xiaoyu007 <422579100@qq.com>
2015-09-14 11:20:54 +0800

    测试一下

---
+++ NotGitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NotGitDemo
+//
+//  Created by Mac on 15/9/14.
+//  Copyright (c) 2015年 jhss(YL). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NotGitDemo/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  NotGitDemo
+//
+//  Created by Mac on 15/9/14.
+//  Copyright (c) 2015年 jhss(YL). All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+  NSLog(@"kajsdlkfjalkdsjflk ");
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+@end
